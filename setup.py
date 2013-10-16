532ec5ed9c75e5e6bebd427fd973f37a273bf5f2
Jens Krause <jens@propertyshelf.com>
2013-10-17 00:55:01 +0700

    package migration to nested namespace

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 version = '1.0'
 
-setup(name='toolkit.color',
+setup(name='theming.toolkit.color',
       version=version,
       description="A product that allows to change a set of theme colors in it's configuration.",
       long_description=open("README.txt").read() + "\n" +
@@ -19,8 +19,9 @@ setup(name='toolkit.color',
       author_email='jens@propertyshelf.com',
       url='http://propertyshelf.com/',
       license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['toolkit'],
+      packages=find_packages('src', exclude=['ez_setup']),
+      namespace_packages=['theming', 'theming.toolkit'],
+      package_dir={'': 'src'},
       include_package_data=True,
       zip_safe=False,
       install_requires=[
